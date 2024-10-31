7407b59dc435310c6bbab54ad40d048f9941ea85
elminhuseynov <elmin.huseynov0821@gmail.com>
2024-10-31 17:14:51 +0400

    lesson2

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = 12345;
+let b  = 98765;
+let result = a * b;
+console.log(result);
+console.log(String(result)[5]);
+
+
+let user = prompt("hansi kursda oxuyursuz?");
+console.log(user.length);
+console.log(user.includes("e"));
+console.log(user.startsWith("code"));
+console.log(user.endsWith("code"));
+console.log(user.replaceAll("t", "####"));
+
+
