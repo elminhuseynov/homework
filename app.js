abb4d5ba24cd06d28c2325e5d2f03f396b2ddbd1
elminhuseynov <elmin.huseynov0821@gmail.com>
2024-11-04 15:57:51 +0400

    homework4

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-let a = 12345;
-let b  = 98765;
-let result = a * b;
+// let a = 12345;
+// let b  = 98765;
+// let result = a * b;
+// console.log(result);
+// console.log(String(result)[5]);
+
+
+// let user = prompt("hansi kursda oxuyursuz?");
+// console.log(user.length);
+// console.log(user.includes("e"));
+// console.log(user.startsWith("code"));
+// console.log(user.endsWith("code"));
+// console.log(user.replaceAll("t", "####"));
+
+// let num = 20;
+// console.log(num != 20);
+// console.log(num < 11);
+// console.log(num == 10);
+// console.log(num === 10);
+// console.log(num <= 10);
+
+// let age = prompt("yasinizi daxil edin")
+// if(age >= 18){
+//     console.log("icaze verildi")
+
+// }else{
+//     console.log("icaze verilmedi");
+    
+// }
+
+let user = prompt("Soyadinizi ve adinizi daxil edin");
+console.log("Salam," + user.split(" ")[1] + "!" );
+
+let num = prompt("4 reqemli eded daxil edin");
+let num1 = (Number(num[0]));
+let num2 = (Number(num[1]));
+let num3 = (Number(num[2]));
+let num4 = (Number(num[3]));
+let result = num1 + num2 + num3 + num4;
 console.log(result);
-console.log(String(result)[5]);
 
 
-let user = prompt("hansi kursda oxuyursuz?");
-console.log(user.length);
-console.log(user.includes("e"));
-console.log(user.startsWith("code"));
-console.log(user.endsWith("code"));
-console.log(user.replaceAll("t", "####"));
+
+
 
 
